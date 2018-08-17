44ee4ff0da1ade062eba5c8940f518d934218a43
Herman Carrasco <herman.carrasco@inacapmail.cl>
2018-08-17 16:14:03 -0300

    Lectura de codigos de barras mostrando el codigo y tipo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//storage
+import { IonicStorageModule } from '@ionic/storage';
+
+//Lector de codigo de barras
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+//Diagnostic
+import { Diagnostic } from '@ionic-native/diagnostic';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +29,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,6 +43,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
+    Diagnostic,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,ToastController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Diagnostic } from '@ionic-native/diagnostic';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,48 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  codigo:string="";
+  format:string="";
 
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner,private diagnostic: Diagnostic,private toastCtrl:ToastController) {
+
+  }
+
+
+  abrirCamara(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.codigo=barcodeData.text;
+      this.format=barcodeData.format;
+
+     }).catch(err => {
+         console.log('Error', err);
+         this.codigo=err;
+     });
+  }
+
+  verificarPermiso(){
+    this.diagnostic.isCameraAuthorized().then((authorized) => {
+      if(authorized)
+          this.abrirCamara()
+      else {
+          this.diagnostic.requestCameraAuthorization().then((status) => {
+              if(status == this.diagnostic.permissionStatus.GRANTED)
+                  this.abrirCamara();
+              else {
+                  // Permissions not granted
+                  // Therefore, create and present toast
+                  this.toastCtrl.create(
+                      {
+                          message: "Cannot access camera", 
+                          position: "bottom",
+                          duration: 5000
+                      }
+                  ).present();
+              }
+          });
+      }
+  });
   }
 
 }
